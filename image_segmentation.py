84695871c5869bf93a3226b14643f0c032813c84
Rahil Shah <45811662+rhlshah@users.noreply.github.com>
2019-12-10 10:01:36 +0530

    Add files via upload

---
+++ image_segmentation.py
@@ -0,0 +1,60 @@
+#Import the libraries
+
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+#Data Preprocess
+im=cv2.imread('elephant.jpg')
+im=cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
+original_shape=im.shape
+
+
+all_pixels=im.reshape((-1,3))
+
+
+#Apply the Algorithm
+
+from sklearn.cluster import KMeans
+dominant_colors=4
+km=KMeans(n_clusters=dominant_colors)
+km.fit(all_pixels)
+centers = km.cluster_centers_
+
+
+centers=np.array(centers,dtype='uint8')
+
+
+i = 1
+
+plt.figure(0,figsize=(8,2))
+
+
+colors = []
+
+for each_col in centers:
+    plt.subplot(1,4,i)
+    plt.axis("off")
+    i+=1
+    
+    colors.append(each_col)
+    
+    
+    a = np.zeros((100,100,3),dtype='uint8')
+    a[:,:,:] = each_col
+    plt.imshow(a)
+
+
+
+
+new_img=np.zeros((330*500,3),dtype='uint8')
+
+
+for ix in range(new_img.shape[0]):
+    new_img[ix]=colors[km.labels_[ix]]
+
+new_img=new_img.reshape(original_shape)
+
+
+plt.imshow(new_img)
+plt.show()
